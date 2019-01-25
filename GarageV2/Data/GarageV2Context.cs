cec593a85efa60212edb8d97c72eb88fdb27be3e
John Lundgren <john.paul.lundgren@gmail.com>
2019-01-25 14:30:21 +0100

    First commit.

---
+++ GarageV2/Data/GarageV2Context.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace GarageV2.Models
+{
+    public class GarageV2Context : DbContext
+    {
+        public GarageV2Context (DbContextOptions<GarageV2Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<GarageV2.Models.ParkedVehicle> ParkedVehicle { get; set; }
+    }
+}
---
+++ GarageV2/Models/ParkedVehicle.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GarageV2.Models
+{
+
+    public enum VehicleType
+    {
+        [Display(Name = "Bil")]
+        Car,
+        [Display(Name = "Båt")]
+        Boat,
+        [Display(Name = "Motorcykel")]
+        Motorbike,
+        [Display(Name = "Flygplan")]
+        Airplane,
+        [Display(Name = "Cykel")]
+        Bicycle
+    }
+
+    public class ParkedVehicle
+    {
+
+        public int Id { get; set; }
+        [Display(Name = "Registreringsnummber")]
+        public string RegNo { get; set; }
+        [Display(Name = "Fordonstyp")]
+        public VehicleType Type { get; set; }
+        [Display(Name = "Färg")]
+        public string Color { get; set; }
+        [Display(Name = "Märke")]
+        public string Brand { get; set; }
+        [Display(Name = "Modell")]
+        public string Model { get; set; }
+        [Display(Name = "Antal däck")]
+        public int NoWheels { get; set; }
+        [Timestamp]
+        public byte[] TimeStamp { get; set; }
+    }
+}
