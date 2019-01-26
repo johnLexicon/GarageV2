0681b7cd49995a881d492b3469f36e33c4e88f34
John Lundgren <john.paul.lundgren@gmail.com>
2019-01-26 15:01:10 +0100

    Added Vehicles mock repository

---
+++ GarageV2/Services/IVehiclesData.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using GarageV2.Models;
+
+namespace GarageV2.Services
+{
+    public interface IVehiclesData
+    {
+        ParkedVehicle AddVehicle(ParkedVehicle vehicle);
+        IEnumerable<ParkedVehicle> GetAll();
+    }
+}
---
+++ GarageV2/Services/VehiclesMockRepository.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using GarageV2.Models;
+
+namespace GarageV2.Services
+{
+    public class VehiclesMockRepository : IVehiclesData
+    {
+
+        List<ParkedVehicle> _parkedVehicles;
+
+        public VehiclesMockRepository()
+        {
+            _parkedVehicles = new List<ParkedVehicle>()
+            {
+                new ParkedVehicle()
+                {
+                    Id = 1,
+                    RegNo = "ABC123",
+                    Type = VehicleType.Airplane,
+                    Brand = "Saab",
+                    Model = "Jet349",
+                    Color = "White",
+                    NoWheels = 2
+                },
+                new ParkedVehicle()
+                {
+                    Id = 2,
+                    RegNo = "CDE223",
+                    Type = VehicleType.Car,
+                    Brand = "Volvo",
+                    Model = "740",
+                    Color = "Green",
+                    NoWheels = 4
+                }
+            };
+        }
+
+        public ParkedVehicle AddVehicle(ParkedVehicle vehicle)
+        {
+            vehicle.Id = _parkedVehicles.Max(v => v.Id) + 1;
+            _parkedVehicles.Add(vehicle);
+
+            return vehicle;
+        }
+
+        public IEnumerable<ParkedVehicle> GetAll()
+        {
+            return _parkedVehicles.OrderBy(v => v.Id);
+        }
+    }
+}
