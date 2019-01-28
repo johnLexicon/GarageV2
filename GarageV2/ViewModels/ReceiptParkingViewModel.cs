d1815bb22694d1e8b266e91736b4b71c75787bed
Per Orest <per.orest@outlook.com>
2019-01-28 11:29:09 +0100

    ViewModels Implemented

---
+++ GarageV2/ViewModels/ReceiptParkingViewModel.cs
@@ -0,0 +1,21 @@
+﻿using GarageV2.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GarageV2.ViewModels
+{
+    public class ReceiptParkingViewModel
+    {
+        public int Id { get; set; }
+        [Display(Name = "Reg-nummer")]
+        public string RegNo { get; set; }
+        [Display(Name = "Fordonstyp")]
+        public VehicleType ParkedVehicleType { get; set; }
+        [Display(Name = "Färg")]
+        public string Color { get; set; }
+        public TimeSpan TimeParked { get; set; }
+    }
+}
