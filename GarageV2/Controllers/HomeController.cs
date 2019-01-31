0499c05416d1e665c0f954fe292a50db1f4dfbd9
John Lundgren <john.paul.lundgren@gmail.com>
2019-01-31 10:24:18 +0100

    Finished commented the HomeController

--- GarageV2/Controllers/HomeController.cs
+++ GarageV2/Controllers/HomeController.cs
@@ -10,6 +10,10 @@ namespace GarageV2.Controllers
 {
     public class HomeController : Controller
     {
+        /// <summary>
+        /// Action used for rendering the start page (home page)
+        /// </summary>
+        /// <returns>The view containing the home page</returns>
         public IActionResult Index()
         {
             return View();
@@ -21,6 +25,10 @@ namespace GarageV2.Controllers
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
 
+        /// <summary>
+        /// Action method used for returning the view containing the custom Error 404 page.
+        /// </summary>
+        /// <returns>The Error404 View</returns>
         public IActionResult Error404()
         {
             return View();
