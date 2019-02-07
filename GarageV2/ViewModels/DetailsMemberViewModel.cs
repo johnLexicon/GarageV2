599883a0f0d47d7868cee4e639ab81d70734b975
John Lundgren <john.paul.lundgren@gmail.com>
2019-02-07 15:30:25 +0100

    Members details functionality.

---
+++ GarageV2/ViewModels/DetailsMemberViewModel.cs
@@ -0,0 +1,19 @@
+﻿using GarageV2.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GarageV2.ViewModels
+{
+    public class DetailsMemberViewModel
+    {
+
+        public string Email { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string PhoneNumber { get; set; }
+        public ICollection<ParkedVehicle> ParkedVehicles { get; set; }
+
+    }
+}
