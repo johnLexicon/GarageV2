41097fcfd7ce8e0baea11d8e6dce452be202aa53
John Lundgren <john.paul.lundgren@gmail.com>
2019-02-20 15:35:55 +0100

    Added Identity Controller views and menu links

---
+++ GarageV2/Controllers/AccountController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GarageV2.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly SignInManager<IdentityUser> _signInManager;
+        private readonly UserManager<IdentityUser> _userManager;
+
+        public AccountController(SignInManager<IdentityUser> signInManager, UserManager<IdentityUser> userManager)
+        {
+            _signInManager = signInManager;
+            _userManager = userManager;
+        }
+
+        //Get
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        //Get
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+
+    }
+}
